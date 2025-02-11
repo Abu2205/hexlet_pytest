e467f603809f05dd8d10c4ffa7ca4291476aa7a5
Abu <abylaikhanturmanov@gmail.com>
2025-02-11 17:47:28 +0000

    Updated exmaple.py file

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,16 @@
 def reverse(string):
-    return string[::-1]
+	"""Reverse string
+	
+	>>> reverse('This is the course about test on Hexlet sxhool!')
+	'!loohxs telxeH no tset tuoba esruoc eht si sihT'
+
+	>>> reverse('I do not understand the task but I will add these results to example.py file!')
+	'!elif yp.elpmaxe ot stluser eseht dda lliw I tub ksat eht dnatsrednu ton od I'
+	"""	
+
+
+	return string[::-1]
+
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
